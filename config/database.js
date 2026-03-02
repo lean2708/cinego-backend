c066b1eeadede4fc1aea6e78b503ca0bea18655c
lean270804@gmail.com <lean270804@gmail.com>
2026-03-02 21:16:27 +0700

    Init project by lean2708

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config(); 
+
+
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || 'postgres', 
+    port: process.env.DB_PORT || 5432,
+    logging: false,
+  }
+);
+
+
+module.exports = sequelize;
---
+++ server.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const sequelize = require('./config/database'); 
+
+
+
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+
+app.use(cors()); 
+app.use(express.json());
+
+
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+
+
+app.listen(PORT, async () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+
+  try {
+    await sequelize.authenticate();
+    console.log('Database connection has been established successfully.');
+
+    await sequelize.sync({ alter: true });
+
+    console.log("SERVER IS READY TO HANDLE REQUESTS !");
+
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+});

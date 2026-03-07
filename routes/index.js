9d89129cf6121fa28c3a125ea5fb0333c29ccde8
dhm261004 <157269973+dhm261004@users.noreply.github.com>
2026-03-07 16:00:05 +0700

    seat API

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const authRouter = require("./authRoute");
 const provinceRouter = require("./provinceRoute");
 const cinemaRouter = require("./cinemaRoute");
+const seatRouter = require("./seatRoute");
 const userRouter = require("./userRoute");
 const express = require("express");
 
@@ -14,6 +15,7 @@ const router = express.Router();
 router.use("/auth", authRouter);
 router.use("/provinces", provinceRouter);
 router.use("/cinemas", cinemaRouter);
+router.use("/seats", seatRouter);
 
 router.use("/users", userRouter);
 
